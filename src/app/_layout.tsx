a20a2b2d2f1f9c7d78720a5b2375a845a315c247
Norlando_jr <norlandojr@hotmail.com>
2026-07-24 20:24:49 -0300

    dowgrade for sdk 54

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,34 +1,28 @@
-import { DefaultTheme, ThemeProvider } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
 import { Stack } from 'expo-router';
-import { useColorScheme } from 'react-native';
-
-import { AnimatedSplashOverlay } from '@/components/animated-icon';
-
-SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-
   return (
-    <ThemeProvider value={DefaultTheme}>
-      <AnimatedSplashOverlay />
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{
+          title: 'Travel Planner',
+        }}
+      />
 
-      <Stack>
-        <Stack.Screen
-          name="index"
-          options={{
-            headerShown: false,
-          }}
-        />
+      <Stack.Screen
+        name="add-trip"
+        options={{
+          title: 'Add Trip',
+        }}
+      />
 
-        <Stack.Screen
-          name="add-trip"
-          options={{
-            title: 'Add New Trip',
-          }}
-        />
-      </Stack>
-    </ThemeProvider>
+      <Stack.Screen
+        name="edit-trip"
+        options={{
+          title: 'Edit Trip',
+        }}
+      />
+    </Stack>
   );
 }
